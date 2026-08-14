95845a4668eeccf02e1e46e9b16dd530eaaeea55
Tharvesh2026 <tharvesh2026@gmail.com>
2026-08-14 18:30:14 +0530

    Final : Daily Commit

--- open-source/openapi-server/src/main/java/com/opensourceapi/server/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.opensourceapi.server.repository;
-
-import com.opensourceapi.server.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
--- open-source/openapi-server/src/main/java/com/opensourceapi/server/repository/RecipeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.opensourceapi.server.repository;
-
-import com.opensourceapi.server.entity.Recipe;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RecipeRepository extends JpaRepository<Recipe, Long> {
-}
